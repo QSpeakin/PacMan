6f297da98688f44654ef5292be1fe8203f8080ce
esabado <esabado@iastate.edu>
2014-03-05 10:48:56 -0600

    Points class updated

--- PacMan/src/game/Points.java
+++ PacMan/src/game/Points.java
@@ -2,4 +2,33 @@ package game;
 
 public class Points {
 
+	private int points;
+	private int previousPoints;
+	private int Totalpoints;
+	
+	public Points(int x){
+		points = x;
+		Totalpoints = x;
+		
+	}
+	
+	public Points(){
+		points = 0;
+	}
+	
+	public void addPoints(int x){
+		previousPoints = points;
+		points = x;
+		Totalpoints += points;
+	}
+	
+	public void undo(){ // You Can only call undo once
+		Totalpoints -= points;
+		points = previousPoints;
+	}
+	
+	public int getPoints(){
+		return Totalpoints;
+		
+	}
 }
